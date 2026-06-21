91de1f12b76f40e7f8675fdf2cdfa791ad668088
Shubham Raj ( Lucky ) <sr7773879@gmail.com>
2026-06-22 00:19:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+document.getElementById('myForm').addEventListener( 'submit' , function(a) {
+	
+	a.preventDefault();
+
+	let f = document.getElementById('fname').value
+	let l = document.getElementById('lname').value
+	let p = document.getElementById('pno').value
+	let e = document.getElementById('gmail').value
+
+	alert(
+		`First Name: ${f} ` +
+		`Last Name: ${l} ` +
+		`Phone Number: ${p} ` +
+		`Email ID: ${e} ` +
+	);
+	
+});
